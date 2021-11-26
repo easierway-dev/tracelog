40dfceb2ed0e7a19024cfabb4d4b5e7f5251d78e
jilong.jian <jilong.jian@mobvista.com>
2021-11-26 14:35:53 +0800

    给日志打标签

--- client.go
+++ client.go
@@ -1,50 +1,95 @@
-package trace_log 
+package trace_log
 
 import (
-    
+    "context"
+    "encoding/json"
+    "fmt"
+    "go.opentelemetry.io/otel/attribute"
+    "unicode/utf8"
 )
 
 /*
-    参考prometheus的client
-        prometheus的label类似于给日志打标签, 比较好用, 后续可以无缝切换到loki等日志系统
+   参考prometheus的client
+       prometheus的label类似于给日志打标签, 比较好用, 后续可以无缝切换到loki等日志系统
 
-    NewLoggerVec 返回 LoggerVec结构体, LoggerVec结构体包含的方法有WithLabel
-    
-    
-    NewTracerLoggerVec【待定】
-    NewTracerVec【待定】
+   NewLoggerVec 返回 LoggerVec结构体, LoggerVec结构体包含的方法有WithLabel
+
+
+   NewTracerLoggerVec【待定】
+   NewTracerVec【待定】
 
 */
 
-type LoggerVec {
-    
+type LoggerOpts Opts
+type Opts struct {
+    Name string
+}
+type LoggerVec struct {
+    opts LoggerOpts
+    labelNames []string
 }
 
-type Logger interface{
+type Loggers interface{
     Log(string)
     LogContext(context.Context, string)
 }
 
 type logger struct {
-
+    Name string
+    Labels map[string]string
+    keyValues []attribute.KeyValue
 }
 
 func NewLoggerVec(opts LoggerOpts, labelNames []string) *LoggerVec {
-    return LoggerVec{
+    for _, val := range labelNames {
+        if !utf8.ValidString(val) {
+            fmt.Errorf("label value %q is not valid UTF-8", val)
+        }
+    }
+    return &LoggerVec{
+        opts: opts,
+        labelNames: labelNames,
     }
 }
 
-func (v *LoggerVec)WithLabelValues(lvs ...string) Logger {
-
+func (v *LoggerVec) WithLabelValues(lvs ...string) Loggers {
+    if len(v.labelNames) != len(lvs){
+        fmt.Errorf("LabelNames value and LabelValues is not equal ")
+        panic(lvs)
+    }
+    for _, val := range lvs {
+        if !utf8.ValidString(val) {
+            fmt.Errorf("label value %q is not valid UTF-8", val)
+        }
+    }
+    LabelValues := make(map[string]string)
+    for  key,_:= range v.labelNames{
+        LabelValues[v.labelNames[key]] = lvs[key]
+    }
+    return &logger{Name:v.opts.Name,Labels: LabelValues}
 }
 
 func (l *logger) Log(logStr string) {
-
+    for key, value := range l.Labels {
+        l.keyValues = append(l.keyValues, attribute.KeyValue{
+            Key:   attribute.Key(key),
+            Value: attribute.StringValue(value),
+        })
+    }
 }
-
+func TraceLog(ctx context.Context, name string, attrs map[string]string, logstr string){
+    indent, _:= json.MarshalIndent(attrs, "", "\t\t")
+    fmt.Printf(
+        "LogInfo{\n\tname: %s,\n\tlogstr: %s,\n\tattrs: %s\n}\n",
+        name,
+        logstr,
+        string(indent),
+    )
+}
 func (l *logger) LogContext(ctx context.Context, logStr string) {
-
-    LogContext(ctx, lostr, attrs)
+    l.Log(logStr)
+    TraceLog(ctx,l.Name,l.Labels,logStr)
+    LogContext(ctx, logStr,l.keyValues)
 }
 
 
