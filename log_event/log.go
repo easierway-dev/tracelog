f01f5b27a21052e9b62c5cf788d422328d4a5947
kexi.cui <kexi.cui@mobvista.com>
2021-11-29 11:53:49 +0800

    add log event

---
+++ log_event/log.go
@@ -0,0 +1,28 @@
+package log_event
+
+
+import (
+    "gitlab.mobvista.com/mtech/tracelog"
+)
+
+type logEventVec interface{
+    WithLabelValues(map[string]string) logEvent
+}
+
+type logEvent interface{
+    Log(string)
+}
+
+func WithContext(ctx context.Context, name string) logEventVec {
+    // if context span sampled
+    // return jaeger_log (hard code)
+    if tracelog.IsSampledFromContext(ctx) {
+    return NewJaegerLogEventVec(ctx, name)
+    }
+
+    // context span not sampled
+    // return nopLogEvent
+    return NewNopLogEventVec()
+}
+
+
