9d7058460f16de940c5eec6e8cef74facfcb9619
kexi.cui <kexi.cui@mobvista.com>
2021-11-29 12:01:26 +0800

    revert tracing_logger

---
+++ tracing_logger_test.go
@@ -0,0 +1,25 @@
+package tracelog
+
+import (
+	"github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestInitTracingLog(t *testing.T) {
+	convey.Convey("test init TracingLog", t, func() {
+		// init tracee
+		traceLogPath := "tracing.log"
+		sampleRatio := 1.0
+		traceConfig, err := NewConfig(WithServiceName("dsp_server"),
+			WithTraceLogPath(traceLogPath),
+			WithSampleRatio(sampleRatio),
+		)
+		if err != nil {
+			panic(err)
+		}
+		if err := Start(traceConfig); err != nil {
+			panic(err)
+		}
+		defer Shutdown(traceConfig)
+	})
+}
