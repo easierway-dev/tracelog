662a35d525ac0e8ca06143382306944dc3eecdc3
kexi.cui <kexi.cui@mobvista.com>
2021-12-13 13:57:55 +0800

    add rpcx plugin

--- instrumentation/rpcx/otelrpcx/client_plugin.go
+++ instrumentation/rpcx/otelrpcx/client_plugin.go
@@ -1,35 +1,46 @@
 package otelrpcx
 
 import (
+    "fmt"
     "context"
     "github.com/smallnest/rpcx/share"
     "go.opentelemetry.io/otel/trace"
+    "go.opentelemetry.io/otel"
 )
 
 // 需要参考opencensus
 type OpenTelemetryClientPlugin struct{}
 
 func (p *OpenTelemetryClientPlugin)PreCall(ctx context.Context, servicePath, serviceMethod string, args interface{}) error{
-    tracer := otel.GetTracerProvider("otelrpcx")
+    fmt.Println("OpenTelemetryClientPlugin precall")
+                 _, okk := ctx.(*share.Context)
+             fmt.Println("OpenTelemetryClientPlugin plugin context rpcxcontext:", okk)
+
+    tracer := otel.GetTracerProvider().Tracer("otelrpcx")
     spanName := "rpcx.client."+servicePath+"."+serviceMethod
-    opsts := []trace.SpanStartOption{
+    opts := []trace.SpanStartOption{
         trace.WithSpanKind(trace.SpanKindServer),
     }
 
-    ctx ,span := tracer.Start(ctx, spanName, opts...)
+    _,span := tracer.Start(ctx, spanName, opts...)
     if rpcxContext, ok := ctx.(*share.Context); ok {
-		rpcxContext.SetValue(share.OpencensusSpanClientKey, span)
-	}
+		rpcxContext.SetValue("otel-rpcx", span)
+        fmt.Println("rpcx context:",rpcxContext.Value("otel-rpcx"))
+    } else {
+        fmt.Println("context not rpcxcontext")
+    }
+        
 	return nil
 
 }
 
 func (p *OpenTelemetryClientPlugin)PostCall(ctx context.Context, servicePath, serviceMethod string, args interface{}, reply interface{}, err error) error {
 	if rpcxContext, ok := ctx.(*share.Context); ok {
-		span1 := rpcxContext.Value(share.OpencensusSpanClientKey)
+		span1 := rpcxContext.Value("otel-rpcx")
 		if span1 != nil {
-			span1.(*trace.Span).End()
+			span1.(trace.Span).End()
 		}
 	}
 	return nil
 }
+
