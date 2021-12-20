dd69826623771e3e064cc794efae8506587a0fba
kexi.cui <kexi.cui@mobvista.com>
2021-12-20 09:18:56 +0800

    gofmt

--- context.go
+++ context.go
@@ -1,30 +1,30 @@
 package tracelog
 
 import (
-    "fmt"
-    "context"
-    "go.opentelemetry.io/otel/trace"
+	"context"
+	"fmt"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func IsSampledFromContext(ctx context.Context) bool {
-         // context invalid, not sample
-     if ctx == nil {
-         return false
-     }
+	// context invalid, not sample
+	if ctx == nil {
+		return false
+	}
 
-     // 从上下文中获取当前span的信息
-     span := trace.SpanFromContext(ctx)
-     return span.SpanContext().IsSampled()
+	// 从上下文中获取当前span的信息
+	span := trace.SpanFromContext(ctx)
+	return span.SpanContext().IsSampled()
 }
 
 func TraceParenetFromContext(ctx context.Context) string {
-    sc := trace.SpanContextFromContext(ctx)
+	sc := trace.SpanContextFromContext(ctx)
 
 	if !sc.IsValid() {
 		return ""
 	}
 
-    flags := sc.TraceFlags() & trace.FlagsSampled
+	flags := sc.TraceFlags() & trace.FlagsSampled
 
 	return fmt.Sprintf(
 		"%.2x-%s-%s-%s",
