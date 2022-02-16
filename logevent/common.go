6a21b605c6e393100303e448b75da283302c8f50
jilong.jian <jilong.jian@mobvista.com>
2022-02-16 13:37:33 +0800

    修改log_event16

--- logevent/common.go
+++ logevent/common.go
@@ -1,16 +1,14 @@
 package logevent
 
 import (
-	"fmt"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/trace"
 	"reflect"
 )
 // 参考链接https://blog.csdn.net/raoxiaoya/article/details/112607719
-// 获取span中的Attributes
+// 获取span中的Resource
 func GetResource(span trace.Span) map[string]string{
-	//var resource1 *resource.Resource
 	m := make(map[string]string)
 	getType := reflect.TypeOf(span)
 	getValue := reflect.ValueOf(span)
@@ -19,33 +17,24 @@ func GetResource(span trace.Span) map[string]string{
 		val := handler.Call([]reflect.Value{})
 		intf := val[0].Interface()    // interface{} 类型
 		resource1 := intf.(*resource.Resource) // bool 类型
-		for key,value:=range resource1.Attributes(){
-			//value := intf.([]attribute.KeyValue)
-			fmt.Printf("Attributes:%d %s %s",key,value.Key,value.Value.AsString())
+		for _,value:=range resource1.Attributes(){
 			m[string(value.Key)] = value.Value.AsString()
 		}
-		fmt.Printf(" %v:\n",m)
 	}
 	return m
 }
-
+// 获取span中的Attributes
 func GetAttributes(span trace.Span) map[string]string{
-	//var resource1 *resource.Resource
 	m := make(map[string]string)
 	getType := reflect.TypeOf(span)
-	fmt.Println("类型具体信息:"+getType.String())
 	getValue := reflect.ValueOf(span)
 	if _, ok := getType.MethodByName("Attributes"); ok {
 		handler := getValue.MethodByName("Attributes")
 		val := handler.Call([]reflect.Value{})
 		intf := val[0].Interface()
-		//keyvalue := intf.([]attribute.KeyValue)
-		for key,value:=range intf.([]attribute.KeyValue){
-			//value := intf.([]attribute.KeyValue)
-			fmt.Printf("Attributes:%d %s %s",key,value.Key,value.Value.AsString())
+		for _,value:=range intf.([]attribute.KeyValue){
 			m[string(value.Key)] = value.Value.AsString()
 		}
-		fmt.Printf(" %v:\n",m)
 	}
 	return m
 }
