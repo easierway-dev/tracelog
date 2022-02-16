8d48a0d6650958eb63a3e5a40fa0598859641a1c
jilong.jian <jilong.jian@mobvista.com>
2022-02-16 13:05:56 +0800

    修改log_event15

--- logevent/common.go
+++ logevent/common.go
@@ -9,44 +9,43 @@ import (
 )
 // 参考链接https://blog.csdn.net/raoxiaoya/article/details/112607719
 // 获取span中的Attributes
+func GetResource(span trace.Span) map[string]string{
+	//var resource1 *resource.Resource
+	m := make(map[string]string)
+	getType := reflect.TypeOf(span)
+	getValue := reflect.ValueOf(span)
+	if _, ok := getType.MethodByName("Resource"); ok {
+		handler := getValue.MethodByName("Resource")
+		val := handler.Call([]reflect.Value{})
+		intf := val[0].Interface()    // interface{} 类型
+		resource1 := intf.(*resource.Resource) // bool 类型
+		for key,value:=range resource1.Attributes(){
+			//value := intf.([]attribute.KeyValue)
+			fmt.Printf("Attributes:%d %s %s",key,value.Key,value.Value.AsString())
+			m[string(value.Key)] = value.Value.AsString()
+		}
+		fmt.Printf(" %v:\n",m)
+	}
+	return m
+}
+
 func GetAttributes(span trace.Span) map[string]string{
-	attributes1 := make(map[string]string)
+	//var resource1 *resource.Resource
+	m := make(map[string]string)
 	getType := reflect.TypeOf(span)
 	fmt.Println("类型具体信息:"+getType.String())
 	getValue := reflect.ValueOf(span)
-	// 反射获取Attributes的属性值
 	if _, ok := getType.MethodByName("Attributes"); ok {
-		m := make(map[string]string)
 		handler := getValue.MethodByName("Attributes")
 		val := handler.Call([]reflect.Value{})
 		intf := val[0].Interface()
-		attributes := intf.([]attribute.KeyValue)
-		for i := 0; i < len(attributes); i++ {
-			value := intf.([]attribute.KeyValue)
-			m[string(value[i].Key)] = value[i].Value.AsString()
-			fmt.Printf("%d %s %s",i,value[i].Key,value[i].Value.AsString())
+		//keyvalue := intf.([]attribute.KeyValue)
+		for key,value:=range intf.([]attribute.KeyValue){
+			//value := intf.([]attribute.KeyValue)
+			fmt.Printf("Attributes:%d %s %s",key,value.Key,value.Value.AsString())
+			m[string(value.Key)] = value.Value.AsString()
 		}
+		fmt.Printf(" %v:\n",m)
 	}
-	return attributes1
-}
-// 获取span中的Resource
-func GetResource(span trace.Span) map[string]string{
-	resources := make(map[string]string)
-	getType := reflect.TypeOf(span)
-	fmt.Println("类型具体信息:"+getType.String())
-	getValue := reflect.ValueOf(span)
-	// 反射获取Resource的属性值
-	if _, ok := getType.MethodByName("Resource"); ok {
-		m := make(map[string]string)
-		handler := getValue.MethodByName("Resource")
-		val := handler.Call([]reflect.Value{})
-		intf := val[0].Interface()
-		resource1 := intf.(*resource.Resource)
-		attributes := resource1.Attributes()
-		for i := 0; i < len(attributes ); i++ {
-			fmt.Printf("%d %s %s",i,attributes[i].Key,attributes[i].Value.AsString())
-			m[string(attributes[i].Key)] = attributes[i].Value.AsString()
-		}
-	}
-	return resources
+	return m
 }
