67369557f9bd1241e9fa6c9899fb7422ca003325
root <root@cffb320b04c7>
2022-03-16 10:58:59 +0800

    use NewBulkProcessorElasticHookWithFunc

--- logevent/hook.go
+++ logevent/hook.go
@@ -35,7 +35,7 @@ func AddES(Url string) *logrus.Logger {
 	// 获取ES的主机地址
 	host := strings.Split(u.Host, ":")
 	// 异步方法，当es出问题，不会影响到主流程的业务
-	hook, err := elogrus.NewAsyncElasticHookWithFunc(client, host[0], log.DebugLevel, GetIndexNameFunc("trace_log"))
+	hook, err := elogrus.NewBulkProcessorElasticHookWithFunc(client, host[0], log.DebugLevel, GetIndexNameFunc("trace_log"))
 	if err != nil {
 		fmt.Println("invalid hook log event:", err.Error())
 		return nil
